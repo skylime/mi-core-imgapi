463f8f5799767fd5c2745f25b790edb632cb3e9f
Thomas Merkel <tm@core.io>
2015-03-01 16:08:24 +0100

    add missing etc

--- copy/var/zoneinit/includes/23-imgapi.sh
+++ copy/var/zoneinit/includes/23-imgapi.sh
@@ -1,7 +1,7 @@
 # Modify config file with hostname
 host=$(hostname)
-sed "s:@SERVER_NAME@:${host}:g" /opt/imgapi/imgapi.config.tpl > \
-	/opt/imgapi/imgapi.config.json
+sed "s:@SERVER_NAME@:${host}:g" /opt/imgapi/etc/imgapi.config.tpl > \
+	/opt/imgapi/etc/imgapi.config.json
 
 # Enable imgapi
 svcadm enable svc:/smartdc/site/imgapi
