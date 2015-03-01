abf793db9ef6991906745cf3121845f19a10ad53
Thomas Merkel <tm@core.io>
2015-03-01 18:37:30 +0100

    generate users and allow mdata info users

--- copy/var/zoneinit/includes/23-imgapi.sh
+++ copy/var/zoneinit/includes/23-imgapi.sh
@@ -1,6 +1,25 @@
-# Modify config file with hostname
+# Create authentication defaults
+IMGAPI_ADMIN=${IMGAPI_ADMIN:-$(mdata-get imgapi_admin 2>/dev/null)} || \
+	IMGAPI_ADMIN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+mdata-put imgapi_admin ${IMGAPI_ADMIN}
+
+# Create authentication for users based on mdata
+if mdata-get imgapi_users 1>/dev/null 2>&1; then
+	for list in $(mdata-get imgapi_users); do
+		u=$(echo ${line} | awk -F \: '{ print $1 }')
+		p=$(echo ${line} | awk -F \: '{ print $2 }')
+
+		USERS="\"${u}\": \"${p}\",\n${USERS}"
+	done
+fi
+
+USERS="${USERS}\n\"admin\": \"${IMGAPI_ADMIN}\""
+
+# Modify config file with hostname and users
 host=$(hostname)
-sed "s:@SERVER_NAME@:${host}:g" /opt/imgapi/etc/imgapi.config.tpl > \
+sed -e "s:@SERVER_NAME@:${host}:g" \
+	-e "s:@AUTH_USERS@:${USERS}:g" \
+	/opt/imgapi/etc/imgapi.config.tpl > \
 	/opt/imgapi/etc/imgapi.config.json
 
 # Enable imgapi
